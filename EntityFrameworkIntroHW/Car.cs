cf13719af085f0da72b7b915dcd1bd25ad83e89c
Muradov2004 <ami65143@gmail.com>
2023-06-01 22:43:56 +0400

    Add project files.

---
+++ EntityFrameworkIntroHW/Car.cs
@@ -0,0 +1,10 @@
+﻿public class Car
+{
+    public int Id { get; set; }
+    public string? Marka { get; set; }
+    public string? Model { get; set; }
+    public int Year { get; set; }
+    public int StateNumber { get; set; }
+
+    public override string ToString() => $"{Id} - {Marka} - {Model} - {Year} - {StateNumber}";
+}
---
+++ EntityFrameworkIntroHW/CarContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class CarContext : DbContext
+{
+    public DbSet<Car> Cars { get; set; }
+    public CarContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(); 
+    }
+}
